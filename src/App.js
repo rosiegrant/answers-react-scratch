591ad5b0fbd1d5092eba9291f1b8367f25a977f9
rosiegrant <rosellen.grant@gmail.com>
2022-03-11 16:16:55 -0500

    other updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import { AnswersHeadlessProvider } from '@yext/answers-headless-react';
 import ParksPage from './pages/ParksPage.tsx';
 function App() {
   return (
     <div className="App">
-      <div>hello</div>
+
       <AnswersHeadlessProvider
         apiKey='06812f79aa06d16a1c6b83d76b5b4350'
         experienceKey='national-parks'
@@ -13,8 +12,8 @@ function App() {
         verticalKey='locations'
       >
 
-        <ParksPage/>
-        
+        <ParksPage />
+
       </AnswersHeadlessProvider>
     </div>
   );
--- src/pages/ParksPage.tsx
+++ src/pages/ParksPage.tsx
@@ -1,14 +1,58 @@
-import { SearchBar } from '@yext/answers-react-components';
+import { LocationBias, SearchBar } from '@yext/answers-react-components';
 import { VerticalResults, StandardCard } from '@yext/answers-react-components';
+import { Filters } from '@yext/answers-react-components';
 
 
 function ParksPage() {
     return (
         <div className="m-6">
-        <SearchBar />
-        <VerticalResults
-            CardComponent={StandardCard}
-        />
+            <div className="flex">
+                <Filters.StaticFilters searchOnChange={false}>
+                    <Filters.FilterGroup>
+                        <Filters.CheckboxOption
+                            key="Vaccinated"
+                            value="true"
+                            label="Vaccinated"
+                            fieldId="fullyVaccinatedStaff"></Filters.CheckboxOption>
+                    </Filters.FilterGroup>
+                </Filters.StaticFilters>
+                <SearchBar />
+            </div>
+
+            <div className="flex">
+                <Filters.Facets searchOnChange={true} className=''>
+                    {facets => facets.map((f, i) => {
+                        if (f.options.length === 0) {
+                            return null;
+                        }
+
+                        return (
+                            <div key={f.fieldId} className='md:w-40 mr-10'>
+                                <Filters.FilterGroup>
+                                    {/* <div>{f.displayName}</div> */}
+                                    <Filters.CollapsibleLabel label={f.displayName} />
+                                    <Filters.CollapsibleSection>
+                                        {/* <Filters.SearchInput /> */}
+                                        {f.options.map(o =>
+                                            <Filters.CheckboxOption
+                                                key={o.displayName}
+                                                value={o.value}
+                                                fieldId={f.fieldId}
+                                            />
+                                        )}
+                                    </Filters.CollapsibleSection>
+                                    {/* {(i < facets.length - 1)} */}
+                                </Filters.FilterGroup>
+                            </div>
+                        );
+                    })}
+                </Filters.Facets>
+                <VerticalResults
+                    CardComponent={StandardCard}
+                />
+            </div>
+            <LocationBias />
+
         </div>
     )
 }
