3efc94a2c48ac6c0d0380f82b3d74b59c8df3e73
rosiegrant <rosellen.grant@gmail.com>
2022-03-16 13:10:06 -0400

    here u go connor!

--- src/pages/ParksPage.tsx
+++ src/pages/ParksPage.tsx
@@ -6,6 +6,7 @@ import { Filters } from '@yext/answers-react-components';
 function ParksPage() {
     return (
         <div className="m-6">
+            <input placeholder="hello" className="placeholder:italic placeholder:text-yellow-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"></input>
             <div className="flex">
                 <Filters.StaticFilters searchOnChange={false}>
                     <Filters.FilterGroup>
